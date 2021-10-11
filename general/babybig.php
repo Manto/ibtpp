e91dd9b3b5be3c66b94dee91b3b65bc5fdcf7581
root <root@ub16.walkingideas.com>
2021-10-11 21:28:26 +0000

    updates package insert

--- general/babybig.php
+++ general/babybig.php
@@ -20,7 +20,7 @@ To date, use of BabyBIG<sup>&reg;</sup> to treat more than 1,500 US infant botul
 For more information, please visit the following links:<br>
 <ul>
 <li><a href="http://www.fda.gov/BiologicsBloodVaccines/BloodBloodProducts/ApprovedProducts/LicensedProductsBLAs/FractionatedPlasmaProducts/ucm089339.htm" target="_blank">"FDA Approval"</a></li>
-<li>"Package Insert for BabyBIG<sup>&reg;</sup>": <a target="_blank" href="/BabyBIG Package Insert 2016_09.pdf">Package Insert for BabyBIG<sup>&reg;</sup></a></li>
+<li>"Package Insert for BabyBIG<sup>&reg;</sup>": <a target="_blank" href="/BabyBIG Package Insert 2021 June.pdf">Package Insert for BabyBIG<sup>&reg;</sup></a></li>
 <li><a href="http://wayback.archive-it.org/7993/20170723024128/https:/www.fda.gov/BiologicsBloodVaccines/BloodBloodProducts/ApprovedProducts/LicensedProductsBLAs/FractionatedPlasmaProducts/ucm117164.htm" target="_blank">"FDA Product Approval Letter"</a><br><br></li>
 </ul>
 </td></tr></table>
