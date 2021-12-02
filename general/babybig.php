769d6f41fdf30265f8b49474b4e2d745eae496eb
root <root@ub16.walkingideas.com>
2021-12-02 21:56:42 +0000

    fix more numbers

--- general/babybig.php
+++ general/babybig.php
@@ -15,7 +15,7 @@ B.
 <br><br>
 The product is a solvent-detergent-treated, sterile, lyophilized powder of immunoglobulin G (IgG), stabilized with 5% sucrose and 1% albumin (human).  It contains no preservative.  The purified immunoglobulin is derived from pooled adult plasma from persons immunized with recombinant botulinum vaccine for serotypes A and B (rBV A/B) who were selected for their high titers of neutralizing antibody against botulinum neurotoxins type A and B.  All donors were tested and found negative for antibodies against the human immunodeficiency virus and the hepatitis B and hepatitis C viruses.  The pooled plasma was fractionated by cold ethanol precipitation of the proteins according to the Cohn/Oncley method and modified to yield a product suitable for intravenous administration.
 <br><br>
-To date, use of BabyBIG<sup>&reg;</sup> to treat more than 2,100 US infant botulism patients has resulted in more than 123 years of avoided hospital stay and more than $130 million of avoided hospital costs.  On average, infant botulism patients have an approximately 3.5 week reduction in time spent in the hospital, resulting in over $97,000 in avoided hospital costs (when compared to the pivotal clinical trial placebo group).
+To date, use of BabyBIG<sup>&reg;</sup> to treat more than 2,100 US infant botulism patients has resulted in more than 123 years of avoided hospital stay and more than $153 million of avoided hospital costs.  On average, infant botulism patients have an approximately 3.5 week reduction in time spent in the hospital, resulting in over $97,000 in avoided hospital costs (when compared to the pivotal clinical trial placebo group).
 <br><br>
 For more information, please visit the following links:<br>
 <ul>
